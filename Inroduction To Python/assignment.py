da1fc6471a4ec435764e25104e0e331c81dc0705
kristen3381 <shavia.laura462@gmail.com>
2024-11-08 17:27:42 +0300

    first commit

--- Inroduction To Python/assignment.py
+++ Inroduction To Python/assignment.py
@@ -1,8 +1,8 @@
-# Ask the user for their name
+
 name = input("What is your name? ")
-# Ask the user for their age (convert input to integer)
+
 age = int(input("How old are you? "))
-# Ask the user for their location
+
 location = input("Where do you live? ")
-# Print a personalized message
+
 print(f"Hello {name}, you are {age} years old I am aware that you live in {location}.Pleasure to meet you!!")
