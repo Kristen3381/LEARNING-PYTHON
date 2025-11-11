9bad9bea9f336cd75e987abc3a39a147cfe9841c
Laura Shavia <shavialaura462@gmail.com>
2025-11-11 22:31:46 +0300

    okpo
    jokjpok

--- Inroduction To Python/assignment.py
+++ Inroduction To Python/assignment.py
@@ -4,5 +4,7 @@ name = input("What is your name? ")
 age = int(input("How old are you? "))
 
 location = input("Where do you live? ")
+print("Hisssss....")
 
 print(f"Hello {name}, you are {age} years old I am aware that you live in {location}.Pleasure to meet you!!")
+print("Hisssssss...")
